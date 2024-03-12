9b526e5ecf29bc94ba2f2d62daed2a3c0ce8df8b
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-03-12 10:48:45 +0100

    Update SimpleManager.bat

--- SimpleManager.bat
+++ SimpleManager.bat
@@ -3,3 +3,8 @@ start Alert.vbs
 
 :menu
 color 0a
+cls
+title ADMINISTRADOR DE TAREAS By ZombieGeek0
+echo -------------------------------------------------------------------------------------------------------------
+echo ADMINISTRADOR DE PROCESOS DE WINDOWS By ZombieGeek0
+echo -------------------------------------------------------------------------------------------------------------
